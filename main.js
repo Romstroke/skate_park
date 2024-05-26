790bf89e5aba1bc03290a161278e9442450e5048
Romina <rominchuela@gmail.com>
2024-05-26 04:04:57 -0400

    carpeta public con css e img, servir html sin handlebars

--- main.js
+++ main.js
@@ -9,6 +9,21 @@ const { Pool } = require("pg");
 const jwt = require('jsonwebtoken');
 // - Express-fileupload --> en collage imagenes (modulo 8 dia 4)
 const expressFileUpload = require("express-fileupload");
+const { dirname } = require('path');
+
+//levantar servidor
+app.listen(3000, () => {
+    console.log('Servidor Express iniciado en el puerto 3000');
+});
+//carpeta public
+app.use(express.static(__dirname + "/public"));
+
+app.set('view engine', 'handlebars');
+
+//sirviendo html sin handlebars 
+app.get('/', (req,res) => {
+    res.sendFile(__dirname + '/index.html')
+});
 
 //plataforma web en la que los participantes se podrán registrar y revisar el estado de su solicitud 
 
