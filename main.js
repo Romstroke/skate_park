822a8ebe1e997f6007b21444d677ba06f032799b
Romina <rominchuela@gmail.com>
2024-05-26 03:22:49 -0400

    Commit inicial

---
+++ main.js
@@ -0,0 +1,12 @@
+// Express
+// - Handlebars --> mercado web (modulo 6 dia 6)
+// - PostgreSQL (pg)
+// - JWT --> en el del alien (fbi modulo 8 dia 8)
+// - Express-fileupload --> en collage imagenes (modulo 8 dia 4)
+
+//plataforma web en la que los participantes se podrán registrar y revisar el estado de su solicitud 
+
+// 1. Crear una API REST con el Framework Express (3 Puntos)  
+// 2. Servir contenido dinámico con express-handlebars (3 Puntos)
+// 3. Ofrecer la funcionalidad Upload File con express-fileupload (2 Puntos) 
+// 4. Implementar seguridad y restricción de recursos o contenido con JWT (2 Puntos)  
