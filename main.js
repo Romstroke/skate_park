35a9241f2a29d3ea887ab35b9a336681b574a4b4
Romina <rominchuela@gmail.com>
2024-05-26 03:41:12 -0400

    Dependencias y .gitignore

--- main.js
+++ main.js
@@ -1,8 +1,14 @@
 // Express
+const express = require('express');
+const app = express();
 // - Handlebars --> mercado web (modulo 6 dia 6)
+const exphbs = require('express-handlebars');
 // - PostgreSQL (pg)
+const { Pool } = require("pg");
 // - JWT --> en el del alien (fbi modulo 8 dia 8)
+const jwt = require('jsonwebtoken');
 // - Express-fileupload --> en collage imagenes (modulo 8 dia 4)
+const expressFileUpload = require("express-fileupload");
 
 //plataforma web en la que los participantes se podrán registrar y revisar el estado de su solicitud 
 
